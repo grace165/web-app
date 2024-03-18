467d3b080e385fb7d4c1533580c9c7212b147733
Grace Elliott <gelliott@eagles.bridgewater.edu>
2024-03-18 00:16:35 -0400

    tyler is SOOO cool and i promise to get him a sexy russian girlfri

--- js/editStudyGroup.js
+++ js/editStudyGroup.js
@@ -24,6 +24,13 @@ const h3 = document.querySelector("h3")
     }]
 }*/
 
+let meetingTime = {
+    "day" : "",
+    "time" : "",
+    "location" : ""
+}
+
+
 meeting_times = []
 
 async function getSGInfo() {
@@ -59,14 +66,14 @@ async function getSGInfo() {
                 document.getElementById("startDate").value = myobj.start_date.substring(0, 10)
                 document.getElementById("endDate").value = myobj.end_date.substring(0, 10)
 
-                //populate meeting times
-
                 if (myobj.is_public == true) {
                     document.getElementById("public").checked = true
                 }
                 else {
                     document.getElementById("private").checked = true
                 }
+
+                //populate meeting times
 
                 var myobj1 = myobj.meeting_times
 
@@ -77,52 +84,50 @@ async function getSGInfo() {
                     }
 
                     const details = document.createElement("label")
-                    const details0 = document.createElement("input")
-
-                    details0.type = "String"
                     details.innerHTML = "Day"
-                    details0.id = "day"
 
+                    const details0 = document.createElement("input")
+                    details0.type = "String"
+                    details0.id = "day"
                     details0.value = myobj1[i].day
 
                     here.appendChild(details)
                     here.appendChild(details0)
 
                     const details1 = document.createElement("label")
-                    const details2 = document.createElement("input")
-
-                    details2.type = "time"
                     details1.innerHTML = "Time"
-                    details2.id = "time"
 
+                    const details2 = document.createElement("input")
+                    details2.type = "time"
+                    details2.id = "time"
                     details2.value = myobj1[i].time
 
                     here.appendChild(details1)
                     here.appendChild(details2)
 
                     const details3 = document.createElement("label")
-                    const details4 = document.createElement("input")
-
-                    details4.type = "String"
                     details3.innerHTML = "Location"
-                    details4.id = "location"
 
+                    const details4 = document.createElement("input")
+                    details4.type = "String"
+                    details4.id = "location"
                     details4.value = myobj1[i].location
 
                     here.appendChild(details3)
                     here.appendChild(details4)
 
-                    const day = document.getElementById("day").value
-                    const time = document.getElementById("time").value
-                    const location = document.getElementById("location").value
+                    //
+                    //let dayForm = document.getElementById("day").value
+                    //let timeForm = document.getElementById("time").value
+                    //let locationForm = document.getElementById("location").value
 
-                    const meetingTime = {
-                        day,
-                        time,
-                        location
+                    meetingTime = {
+                        "day" : myobj1[i].day,
+                        "time" : myobj1[i].time,
+                        "location" : myobj1[i].location
                     }
 
-                    meeting_times.push(meetingTime)
+                    meeting_times[i] = meetingTime
                 }
             }
         }
@@ -145,8 +150,23 @@ document.getElementById("saveChangesBtn").addEventListener('click', async (event
     const start_date = document.getElementById("startDate").value
     const end_date = document.getElementById("endDate").value
 
-    //const url = new URL("http://127.0.0.1:3000/studygroup/" + studyGroupID)
-    const url = new URL("https://api-server-1.azurewebsites.net/studygroup/" + studyGroupID)
+    var dayArr = document.querySelectorAll("input[id='day']")
+    var timeArr = document.querySelectorAll("input[id='time']")
+    var locationArr = document.querySelectorAll("input[id='location']")
+    
+    for(const key in meeting_times){
+        //console.log(meeting_times[key])
+        meeting_times[key].day = dayArr[key].value
+        meeting_times[key].time = timeArr[key].value
+        meeting_times[key].location = locationArr[key].value
+        
+    }
+    console.log(meeting_times)
+
+    //console.log(dayArr)
+
+    const url = new URL("http://127.0.0.1:3000/studygroup/" + studyGroupID)
+    //const url = new URL("https://api-server-1.azurewebsites.net/studygroup/" + studyGroupID)
 
     const data = {
         name,
@@ -157,7 +177,7 @@ document.getElementById("saveChangesBtn").addEventListener('click', async (event
         max_participants,
         start_date,
         end_date,
-        //meeting_times
+        meeting_times
     }
 
     console.log(data)
